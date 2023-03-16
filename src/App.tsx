e62274e9351506702b738083eb96dc18ee5bd35a
Rogelio Morrell <molekilla@gmail.com>
2023-03-16 10:06:34 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,19 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { QuillBinding } from "y-quill";
 
+let binding: any;
 // Quill.register("modules/cursors", QuillCursors);
 const ydoc = new Y.Doc();
 const text = ydoc.getText("quill");
 function App() {
   const quillRef = useRef(null);
   const [value, setValue] = useState("");
-  const [binding, setBinding] = useState({});
-  const [binded, setBinded] = useState(false);
 
-  // if (quillRef.current && !binded) {
-  //   console.log("binding", quillRef.current);
-  //   // @ts-ignore - quillRef.current is not null
-  //   setBinding(new QuillBinding(text, quillRef.current.getEditor()));
-  //   setBinded(true);
-  // }
+  if (quillRef.current && binding === undefined) {
+    console.log("binding", quillRef.current);
+    // @ts-ignore - quillRef.current is not null
+    binding = new QuillBinding(text, quillRef.current.getEditor());
+  }
   return (
     <ReactQuill theme="snow" value={value} onChange={setValue} ref={quillRef} />
   );
