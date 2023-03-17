0e01b0824fde3f3077ddd7ed8b807e7669bd6f0e
Rogelio Morrell <molekilla@gmail.com>
2023-03-17 16:07:45 -0500

    syncs

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,23 @@ import "./App.css";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { QuillBinding } from "y-quill";
-import { FdpStoragePersistence } from "y-fdp-storage";
-import { Bee } from "@ethersphere/bee-js";
+import { makePrivateKeySigner, FdpStoragePersistence } from "y-fdp-storage";
+import { Bee, Utils } from "@ethersphere/bee-js";
 import { ethers } from "ethers";
 
-// TODO: Ask for Postage Batch ID
 const postageBatchId =
   process.env.BEE_POSTAGE ||
   "1c082c5e642e15d49b6689f5437c2eb9e6aa9c546a8ed1d11d0024b043bca371";
 
-// TODO: Ask for Bee API URL
-const bee = new Bee("http://localhost:1633");
-
-const testIdentity = {
-  privateKey:
-    "634fb5a872396d9693e5c9f9d7233cfa93f395c093371017ff44aa9ae6564cdd",
-  publicKey:
-    "03c32bb011339667a487b6c1c35061f15f7edc36aa9a0f8648aba07a4b8bd741b4",
-  address: "8d3766440f0d7b949a5e32995d09619a7f86e632",
-};
+const bee = new Bee(process.env.REACT_APP_BEE_URL || "http://localhost:1633");
 
 // TODO: Wallet TODO
-const wallet = ethers.Wallet.createRandom();
-const topic = "/crdt/document/test";
+const wallet = makePrivateKeySigner(
+  Utils.hexToBytes(
+    "634fb5a872396d9693e5c9f9d7233cfa93f395c093371017ff44aa9ae6564cdd"
+  )
+);
+const topic = "/crdt/document/test1";
 
 // Create FdpStoragePersistence object
 const persistence = new FdpStoragePersistence(
@@ -39,10 +33,10 @@ const persistence = new FdpStoragePersistence(
 let binding: any;
 
 const ydoc = new Y.Doc();
-ydoc.on("update", (update) => {
-  console.log("update", update);
-  persistence.storeUpdate(update);
+ydoc.on("update", async (update) => {
+  await persistence.storeUpdate(update);
 });
+const close = persistence.subscribe(ydoc);
 
 const text = ydoc.getText("quill");
 function App() {
@@ -54,6 +48,9 @@ function App() {
     // @ts-ignore - quillRef.current is not null
     binding = new QuillBinding(text, quillRef.current.getEditor());
   }
+
+  // long polling
+
   return (
     <ReactQuill theme="snow" value={value} onChange={setValue} ref={quillRef} />
   );
